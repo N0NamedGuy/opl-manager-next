a3130b4b204436bc06ad786eb9b8ac6787dbea87
David Serrano <dserrano@linkare.com>
2020-02-13 01:16:41 +0100

    A heck of a lot of work to make POPSManager kinda work

--- src/components/POPSManager.jsx
+++ src/components/POPSManager.jsx
@@ -4,18 +4,22 @@ import { remote } from 'electron';
 
 import { readdir } from 'fs';
 import path from 'path';
+import fs from 'fs';
 import tmp from 'tmp';
 
+import { exec } from 'child_process';
+
 import AppSettingsContext from '../contexts/AppSettingsContext.jsx';
+import { getPSXGameId } from '../utils/cuehelper.js';
 
 const POPSManager = () => {
     const AppSettings = useContext(AppSettingsContext);
 
+    const { oplRoot, cue2popsBin } = AppSettings.settings;
+
     const [fileList, setFileList] = useState([]);
 
     useEffect(() => {
-        const oplRoot = AppSettings.settings && AppSettings.settings.oplRoot;
-
         if (oplRoot) {
             readdir(path.resolve(oplRoot, 'POPS'), (err, files) => {
                 if (!err) {
@@ -28,19 +32,73 @@ const POPSManager = () => {
     });
 
     function addBinCue() {
-        const oplRoot = AppSettings.settings && AppSettings.settings.oplRoot;
-
-        if (oplRoot) {
+        if (oplRoot && cue2popsBin) {
             const popsPath = path.resolve(oplRoot, 'POPS');
 
             remote.dialog.showOpenDialog({
-                properties: ["openFile"]
-            }).then((file) => {
-                tmp.dir((error, tmpDir, removeCb) => {
-                    const tempVcd = path.resolve(tmpDir, 'temp.vcd');
-                    //
-                    removeCb();
-                });
+                properties: ["openFile"],
+                filters: [{ extensions: ['cue'], name: 'CUE sheets' }]
+            }).then(({ filePaths, canceled }) => {
+                if (!canceled) {
+                    const cuePath = filePaths[0];
+
+                    getPSXGameId(cuePath).then((gameId) => {
+                        console.log('Got game id', gameId);
+
+                        tmp.dir((error, tmpDir, removeCb) => {
+                            const cueName = path.parse(cuePath).name;
+                            const vcdName = `${gameId}.${cueName}`;
+                            const vcdFileName = `${vcdName}.VCD`;
+                            const vcdPath = path.resolve(tmpDir, vcdFileName);
+                            const vcdPathInPops = path.resolve(popsPath, vcdFileName);
+                            const elfPathInPops = path.resolve(popsPath, `${vcdName}.ELF`);
+                            const cmd = `${cue2popsBin} "${cuePath}" "${vcdPath}"`;
+
+                            const popstartPath = path.resolve(popsPath, 'POPSTARTER.ELF');
+
+                            console.log('Executing', cmd);
+
+                            exec(cmd, (error, stdout, stderr) => {
+                                // cue2pops returns error if things go well??
+                                if (error) {
+                                    if (error.code === 1) {
+
+                                        console.log('cue2pops done', stdout);
+                                        //removeCb();
+                                        console.log('Check dir ', tmpDir);
+
+                                        fs.stat(vcdPath, (err, stat) => {
+                                            const fileSize = stat.size;
+                                            let bytesCopied = 0;
+
+                                            fs.createReadStream(vcdPath)
+                                                .on('data', (buffer) => {
+                                                    bytesCopied += buffer.length;
+
+                                                    const percentage = ((bytesCopied / fileSize) * 100).toFixed(2);
+                                                    console.log('Copied ', percentage);
+                                                })
+                                                .on('end', () => {
+                                                    console.log('finished copying', vcdPathInPops);
+                                                    fs.createReadStream(popstartPath)
+                                                        .on('end', () => {
+                                                            console.log('Game ready to be played');
+                                                            removeCb();
+                                                        })
+                                                        .pipe(fs.createWriteStream(elfPathInPops));
+                                                })
+                                                .pipe(fs.createWriteStream(vcdPathInPops));
+                                        })
+
+                                    } else {
+                                        console.error('cue2pops error', stderr);
+                                    }
+                                }
+
+                            })
+                        });
+                    });
+                }
             });
         }
     }
---
+++ src/utils/cuehelper.js
@@ -0,0 +1,34 @@
+import { createReadStream } from 'fs';
+import { parse as cueParse } from 'cue-parser';
+import { resolve as pathResolve, dirname } from 'path';
+
+export function getPSXGameId(cueFile) {
+    return new Promise((resolve, reject) => {
+        const cueSheet = cueParse(cueFile);
+        const binFile = cueSheet.files.find((file) =>
+            file.type.toUpperCase() === 'BINARY'
+        );
+
+        const stream = createReadStream(pathResolve(dirname(cueFile), binFile.name), {
+            start: 0x9340,
+            end: 0x935f
+        });
+
+        let gameIdBuf = '';
+
+        stream
+            .on('error', (error) => {
+                reject(error);
+            })
+            .on('data', (data) => {
+                gameIdBuf += data;
+            })
+            .on('close', () => {
+                const gameIdBufTrimmed = gameIdBuf.toString().trim();
+                const gameId =
+                    `${gameIdBufTrimmed.substring(0, 8)}.${gameIdBufTrimmed.substring(8)}`;
+
+                resolve(gameId);
+            });
+    });
+}
